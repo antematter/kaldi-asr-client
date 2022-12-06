0ed626eb5d0e152ddf0bfb2289ea0fac5d3e104d
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2022-12-06 19:55:44 +0530

    minimal_client.cc: add actual API implementation

--- minimal_client.cc
+++ minimal_client.cc
@@ -18,9 +18,10 @@ enum {
 };
 
 struct client {
-  TritonASRClient asr_client;
   std::vector<kaldi::WaveData> inputs;
   std::vector<std::string> outputs;
+  size_t expected_inputs;
+  size_t iter_idx;
 };
 
 struct membuf : std::streambuf {
@@ -60,14 +61,82 @@ int feed_wav(TritonASRClient &asr_client, kaldi::WaveData &wave_data) {
   return 0;
 }
 
-int feed_wav_from_charbuf(TritonASRClient &asr_client, char *buf, size_t len) {
-  membuf sbuf(buf, buf + len);
+extern "C" {
+struct client *client_alloc(void) {
+  struct client *client = new struct client;
+  return client;
+}
+
+void client_destroy(struct client *client) { delete client; }
+
+int client_infer_begin(struct client *client, size_t len) {
+  client->iter_idx = 0;
+  client->expected_inputs = len;
+
+  client->inputs.clear();
+  client->outputs.clear();
+
+  client->inputs.reserve(client->expected_inputs);
+  client->outputs.reserve(client->expected_inputs);
+
+  return 0;
+}
+
+int client_infer_feed(struct client *client, char *bytes, size_t len) {
+  membuf sbuf(bytes, bytes + len);
   std::istream is(&sbuf);
 
-  kaldi::WaveData wave_data;
-  wave_data.Read(is);
+  client->inputs.emplace_back();
+  client->inputs[client->inputs.size() - 1].Read(is);
 
-  return feed_wav(asr_client, wave_data);
+  return 0;
+}
+
+int client_infer_perform(struct client *client) {
+  if (client->expected_inputs != client->inputs.size()) {
+    std::cerr << "Expected " << client->expected_inputs << " inputs but got "
+              << client->inputs.size() << "!\n";
+    std::exit(1);
+  }
+
+  if (client->inputs.size() == 0) {
+    std::cerr << "No inputs fed!" << '\n';
+    std::exit(1);
+  }
+
+  kaldi::WaveData &wave_data = client->inputs[0];
+
+  float samp_freq = wave_data.SampFreq();
+  double duration = wave_data.Duration();
+
+  std::cout << "Loaded file with frequency " << samp_freq << "hz, duration "
+            << duration << '\n';
+
+  TritonASRClient asr_client(URL, MODEL, NCLIENTS, true, false, false,
+                             samp_freq);
+
+  feed_wav(asr_client, wave_data);
+  asr_client.WaitForCallbacks();
+
+  if (client->outputs.size() != client->inputs.size()) {
+    std::cerr << "Outputs (" << client->outputs.size()
+              << ") not equal to Inputs (" << client->inputs.size() << ")\n";
+    std::exit(1);
+  }
+
+  return 0;
+}
+
+/* Iterator since it's not possible to return non-trivial types without using
+ * libpython. */
+const char *client_infer_output(struct client *client) {
+  if (client->iter_idx >= client->outputs.size()) {
+    client->iter_idx = 0;
+    return NULL;
+  }
+
+  return client->outputs[client->iter_idx++].c_str();
+}
 }
 
 int main(int argc, char *const argv[]) {
