32e657658acf83bb12817be9e9a2b0f1c0f364bf
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2022-12-11 21:40:18 +0530

    scripts/install_deps.sh: add python3-wheel-whl for bdist_wheel

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo apt install -y build-essential patchelf cmake zlib1g-dev make automake autoconf patch grep bzip2 gzip unzip wget git sox gfortran libtool intel-mkl python3 
+sudo apt install -y build-essential patchelf cmake zlib1g-dev make automake autoconf patch grep bzip2 gzip unzip wget git sox gfortran libtool intel-mkl python3 python3-wheel-whl
